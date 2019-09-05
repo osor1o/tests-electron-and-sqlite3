9d5f507e5ab08853abb9766e46119f19c901aee3
Osorio Neto <osor1o@hotmail.com>
2019-09-05 04:02:39 -0300

    added sqlite

--- renderer.js
+++ renderer.js
@@ -1,3 +1,23 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+// var sqlite3 = require('sqlite3').verbose();
+// var db = new sqlite3.Database('mydb.db');
+
+db.serialize(function() {
+    db.run("CREATE TABLE if not exists lorem (info TEXT)");
+
+    var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+    for (var i = 0; i < 10; i++) {
+        stmt.run("Ipsum " + i);
+    }
+    stmt.finalize();
+
+    db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+        console.log(row.id + ": " + row.info);
+    });
+});
+
+db.close();
+
+localStorage.setItem("state", "{nome: osorio}")
